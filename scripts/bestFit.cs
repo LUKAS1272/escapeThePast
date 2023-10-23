678d5266dc60acbc5cce0c2800fcae2499eede14
LUKAS1272 <zdarsky1272@gmail.com>
2023-10-23 19:09:20 +0200

    added scalable numbers and edited button behavior

---
+++ scripts/bestFit.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class bestFit : Label
+{	public override void _Ready() {
+		// Text = "Ahoj";
+
+		// int textLength = Text.Length;
+		// float maxWidth = RectSize.x;
+		// float finalSize = maxWidth / textLength;
+
+		// DynamicFont preset = (DynamicFont)Get("custom_fonts/font");
+		// preset.Size = (int)finalSize;
+	}
+}
--- scripts/gridFiller.cs
+++ scripts/gridFiller.cs
@@ -13,11 +13,13 @@ public class gridFiller : GridContainer {
 			for (int y = 0; y < Columns; y++) {
 				ColorRect instance;
 
-				if (y % 2 == 0) { instance = (ColorRect)gridBlock.Instance(); }
-				else { instance = (ColorRect)puzzleBlock.Instance(); }
-
+				instance = (ColorRect)gridBlock.Instance();
 				instance.RectMinSize = cellSize;
 				AddChild(instance);
+				Label instanceLabel = (Label)instance.GetChild(0).GetChild(0);
+				
+				DynamicFont preset = (DynamicFont)instanceLabel.Get("custom_fonts/font");
+				preset.Size = (int)(instanceLabel.RectSize.x / instanceLabel.Text.Length * 0.65);
 			}
 		}
 	}
